73fb7b8bf2980899222029295c9082be982083cf
Kate Sullivan <sullivan.kate.e@gmail.com>
2023-11-26 19:39:23 -0800

    ?

---
+++ cmake-build-debug/CMakeFiles/TermProjectGroup8.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TermProjectGroup8.dir/implementation.c.obj"
+  "CMakeFiles/TermProjectGroup8.dir/implementation.c.obj.d"
+  "CMakeFiles/TermProjectGroup8.dir/main.c.obj"
+  "CMakeFiles/TermProjectGroup8.dir/main.c.obj.d"
+  "TermProjectGroup8.exe"
+  "TermProjectGroup8.exe.manifest"
+  "TermProjectGroup8.pdb"
+  "libTermProjectGroup8.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TermProjectGroup8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TermProjectGroup8.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for TermProjectGroup8.
+# This may be replaced when dependencies are built.
