357ecf929687487b0d7a33f7eea6c957ecd2f3e3
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-11-27 15:36:08 -0800

    commit

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/yueyu/Desktop/COMP2510TermProject
+# Install script for directory: /Users/oceaanpendharkar/CLionProjects/COMP2510TermProject
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/yueyu/Desktop/COMP2510TermProject/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/oceaanpendharkar/CLionProjects/COMP2510TermProject/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
